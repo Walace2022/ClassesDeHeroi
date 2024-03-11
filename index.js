c7dd0a7dd157fb35783e1373453c8f9d7f5796e0
Henrique Walace <Henriquewalace@hotmail.com>
2024-03-11 18:48:29 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")();
+
 class heroi {
   constructor(nome, idade, tipo) {
     this.nome = nome;
@@ -19,6 +21,36 @@ class heroi {
 
     console.log(`O ${this.tipo} atacou usando ${ataque}!`);
   }
+
+  apresentar() {
+    console.log(`Eu sou ${nome}, tenho ${idade} anos e sou um ${tipo}.`);
+  }
 }
 
 const mago = new heroi("Mago", 60, "mago");
+const guerreiro = new heroi("Guerreiro", 60, "guerreiro");
+const monge = new heroi("Monge", 60, "monge");
+const ninja = new heroi("Ninja", 60, "ninja");
+
+let nome = prompt("Qual o nome do seu herói? ");
+console.log("\n");
+let idade = prompt("qual a idade do seu herói? ");
+console.log("\n");
+
+console.log("Cada herói ataca de maneira diferente como: ");
+
+mago.atacar();
+guerreiro.atacar();
+monge.atacar();
+ninja.atacar();
+
+console.log("\n");
+let tipo = prompt(
+  "Qual deles seu herói vai ser?(mago, guerreiro, monge, ninja) "
+);
+
+let heroiPersonalizado = new heroi(nome, idade, tipo);
+
+console.log("\n");
+heroiPersonalizado.apresentar();
+heroiPersonalizado.atacar();
