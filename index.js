2005d55dd70b929c861f7f31ed05c2a4d95d79fc
Henrique Walace <Henriquewalace@hotmail.com>
2024-03-11 18:37:51 -0300

    Criado classe e instanciado objetos para teste

---
+++ index.js
@@ -0,0 +1,24 @@
+class heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+    if (this.tipo === "mago") {
+      ataque = "magia";
+    } else if (this.tipo === "guerreiro") {
+      ataque = "espada";
+    } else if (this.tipo === "monge") {
+      ataque = "artes marciais";
+    } else {
+      ataque = "shuriken";
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}!`);
+  }
+}
+
+const mago = new heroi("Mago", 60, "mago");
